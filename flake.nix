4e5b50a4fddd458d6dbdc4738b77e276d60a7698
Julien Langlois <yourstruly@julienlanglois.me>
2024-05-18 15:51:52 -0400

    Add nix tooling

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in pkgs.rustPlatform.buildRustPackage {
+      pname = "tuigreet";
+      version = "0.9.1";
+      src = ./.;
+      cargoLock.lockFile = ./Cargo.lock;
+    };
+    devShells.x86_64-linux.default =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in self.packages.x86_64-linux.default.overrideAttrs(old: {
+        nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.rust-analyzer ];
+      });
+    overlays.default = final: prev: {
+      greetd = prev.greetd // {
+        tuigreet = self.packages.x86_64-linux.default;
+      };
+    };
+  };
+}
